22557382dba96c3f1473d71b3ddb75013ee4fde7
mcci <mcristina.carmona@gmail.com>
2018-06-07 11:19:15 -0500

    Create template code to fill data into database

---
+++ Literature search/R/Fill database.R
@@ -0,0 +1,23 @@
+#########################################################
+#Fill in data into database
+#########################################################
+#Load datasets
+
+setwd("C:/Users/cris.carmona/Documents/MEGAsync/Projects/Post-doc/Riesgo de extinción y selección sexual/extinction_data/Literature search/input")
+
+csvfiles <- list.files(path = ".", pattern='*\\.csv$', all.files=TRUE)
+csvfiles
+
+import.list <- lapply(csvfiles, read.csv, header = TRUE, as.is=TRUE, na.strings=c("NA","na","N","-","---"," ","",".","sin dato","SD","sd","Sin Dato", -999,"-999"))
+
+
+#str(import.list)
+ls()
+
+working.list <- import.list
+names(working.list) <- c("db")
+
+attach(working.list)
+###########################################################
+
+names(db)
