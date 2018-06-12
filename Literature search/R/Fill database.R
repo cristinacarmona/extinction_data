e8ce499edacd2092a13a90e93ab2241775a53c67
mcci <mcristina.carmona@gmail.com>
2018-06-12 18:12:59 -0500

    Example with reptiles

--- Literature search/R/Fill database.R
+++ Literature search/R/Fill database.R
@@ -14,10 +14,22 @@ import.list <- lapply(csvfiles, read.csv, header = TRUE, as.is=TRUE, na.strings=
 #str(import.list)
 ls()
 
-working.list <- import.list
-names(working.list) <- c("db")
+working.list <- import.list[2]
+names(working.list) <- c("db2")
 
 attach(working.list)
 ###########################################################
 
-names(db)
+names(db2)
+
+#Example with reptiles--------------
+#Choose Class to work with
+table(db2$Class)
+
+str(db2[db2$Class %in% "REPTILIA",]) #6278 obs
+
+db.rep <- db2[db2$Class %in% c("REPTILIA") & !is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm),]
+
+str(db.rep)#161 obs
+
+db.rep[1:5,"binomial"]
