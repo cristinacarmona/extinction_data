117f784902b9127976e05f33cc80643d406a6b84
cristinacarmona <mcristina.carmona@gmail.com>
2018-06-13 17:56:23 -0500

    added info for 15 reptiles

--- Literature search/R/Fill database.R
+++ Literature search/R/Fill database.R
@@ -5,31 +5,52 @@
 
 setwd("C:/Users/cris.carmona/Documents/MEGAsync/Projects/Post-doc/Riesgo de extinción y selección sexual/extinction_data/Literature search/input")
 
-csvfiles <- list.files(path = ".", pattern='*\\.csv$', all.files=TRUE)
-csvfiles
+xlsxfiles <- list.files(path = ".", pattern='*\\.xlsx$', all.files=TRUE)
+xlsxfiles
 
-import.list <- lapply(csvfiles, read.csv, header = TRUE, as.is=TRUE, na.strings=c("NA","na","N","-","---"," ","",".","sin dato","SD","sd","Sin Dato", -999,"-999"))
+library(openxlsx)
+import.list <- lapply(xlsxfiles, read.xlsx, sheet=1, colNames = TRUE, na.strings=c("NA","na","N","-","---"," ","",".","sin dato","SD","sd","Sin Dato", -999,"-999"))
 
 
 #str(import.list)
 ls()
 
-working.list <- import.list[2]
-names(working.list) <- c("db2")
+working.list <- import.list
+names(working.list) <- c("db")
 
 attach(working.list)
 ###########################################################
 
-names(db2)
+names(db)
 
 #Example with reptiles--------------
 #Choose Class to work with
-table(db2$Class)
+table(db$Class)
 
-str(db2[db2$Class %in% "REPTILIA",]) #6278 obs
+str(db[db$Class %in% "REPTILIA",]) #6278 obs
 
-db.rep <- db2[db2$Class %in% c("REPTILIA") & !is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm),]
+db.rep <- db[db$Class %in% c("REPTILIA") & !is.na(db$male_svl_cm) & !is.na(db$female_svl_cm),]
 
 str(db.rep)#161 obs
 
 db.rep[1:5,"binomial"]
+db.rep[1:5,]
+#[1] "Acanthodactylus erythrurus" 13-jun-2018  mating system and ASR
+#"Acanthophis laevis"   no data available (CCI 13/06/2018)
+#[3] "Acanthophis rugosus"        no data available (CCI 13/06/2018)
+#"Ameiva atrigularis"        no data available (CCI 13/06/2018)
+#[5] "Ameiva praesignis" no data available (CCI 13/06/2018)
+
+db.rep[6:10,"binomial"]
+#[1] "Anilios ammodytes" no data available (CCI 13/06/2018) 
+#"Anilios australis"     no data available (CCI 13/06/2018) 
+#[3] "Anilios bituberculatus"  no data available (CCI 13/06/2018) 
+#"Anilios diversus"      no data available (CCI 13/06/2018) 
+#[5] "Anilios endoterus"   no data available (CCI 13/06/2018) 
+
+db.rep[11:15,"binomial"]
+#[1] "Anilios ligatus"     no data available (CCI 13/06/2018) 
+#"Anilios pinguis"    no data available (CCI 13/06/2018) 
+#[3] "Anolis carolinensis"  ASR and Mating system already known from adw
+#"Antaresia childreni"  no data available(CCI 13/06/2018)
+#[5] "Antaresia maculosa"  no data available (CCI 13/06/2018)
