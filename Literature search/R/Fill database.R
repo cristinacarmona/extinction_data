9513d8740177a25c5923b76d4467d9163acfeb20
Pepi Girón <peepig12@gmail.com>
2018-06-27 14:13:34 -0500

    busqueda de datos para 5 especies

--- Literature search/R/Fill database.R
+++ Literature search/R/Fill database.R
@@ -2,34 +2,59 @@
 #Fill in data into database
 #########################################################
 #Load datasets
+setwd("C:/Users/peepi_000/Documents/extinction_data/Literature search/input")
 
-setwd("C:/Users/cris.carmona/Documents/MEGAsync/Projects/Post-doc/Riesgo de extinción y selección sexual/extinction_data/Literature search/input")
+xlsxfiles <- list.files(path = ".", pattern='*\\.xlsx$', all.files=TRUE)
+xlsxfiles
 
-csvfiles <- list.files(path = ".", pattern='*\\.csv$', all.files=TRUE)
-csvfiles
-
-import.list <- lapply(csvfiles, read.csv, header = TRUE, as.is=TRUE, na.strings=c("NA","na","N","-","---"," ","",".","sin dato","SD","sd","Sin Dato", -999,"-999"))
+library(openxlsx) #CERRAR ARCHIVO "merged_iucn" en excel antes de correr codigo
+import.list <- lapply(xlsxfiles, read.xlsx, sheet=1, colNames = TRUE, na.strings=c("NA","na","N","-","---"," ","",".","sin dato","SD","sd","Sin Dato", -999,"-999"))
+#import.list <- read.xlsx("merged_iucn_and_db_v4.xlsx", sheet=1, colNames=TRUE, na.strings=c("NA","na","N","-","---"," ","",".","sin dato","SD","sd","Sin Dato", -999,"-999"))
 
 
 #str(import.list)
 ls()
 
-working.list <- import.list[2]
-names(working.list) <- c("db2")
+working.list <- import.list
+names(working.list) <- c("db")
 
 attach(working.list)
 ###########################################################
 
-names(db2)
+names(db)
 
 #Example with reptiles--------------
 #Choose Class to work with
-table(db2$Class)
+table(db$Class)
 
-str(db2[db2$Class %in% "REPTILIA",]) #6278 obs
+str(db[db$Class %in% "MAMMALIA",]) #5674 obs
 
-db.rep <- db2[db2$Class %in% c("REPTILIA") & !is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm),]
+db.mam <- db[db$Class %in% c("MAMMALIA") & !is.na(db$male_body_mass_g) & !is.na(db$female_body_mass_g),]
 
-str(db.rep)#161 obs
+str(db.mam)#1174 obs
 
-db.rep[1:5,"binomial"]
+db.mam[1:5,"binomial"]
+db.rep[1:5,]
+# [1] "Acomys cahirinus"  #no data  27 06 2018 PG
+# [2] "Acomys russatus"     #no data
+# [3] "Acomys spinosissimus" #no data
+# [4] "Acomys subspinosus"  #no data
+# [5] "Acomys wilsoni"  #no data
+
+db.rep[6:10,"binomial"]
+#[1] "Anilios ammodytes" no data available (CCI 13/06/2018) 
+#"Anilios australis"     no data available (CCI 13/06/2018) 
+#[3] "Anilios bituberculatus"  no data available (CCI 13/06/2018) 
+#"Anilios diversus"      no data available (CCI 13/06/2018) 
+#[5] "Anilios endoterus"   no data available (CCI 13/06/2018) 
+
+db.rep[11:15,"binomial"]
+#[1] "Anilios ligatus"     no data available (CCI 13/06/2018) 
+#"Anilios pinguis"    no data available (CCI 13/06/2018) 
+#[3] "Anolis carolinensis"  ASR and Mating system already known from adw
+#"Antaresia childreni"  no data available(CCI 13/06/2018)
+#[5] "Antaresia maculosa"  no data available (CCI 13/06/2018)
+
+
+
+
