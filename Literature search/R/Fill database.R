7c5d43beff75022c5a723aa48ac50b0630060e79
Pepi Girón <peepig12@gmail.com>
2018-08-07 23:39:16 -0500

    se trabajo

--- Literature search/R/Fill database.R
+++ Literature search/R/Fill database.R
@@ -63,6 +63,24 @@ db.rep[11:15,"binomial"]
 #"Antaresia childreni"  no data available(CCI 13/06/2018)
 #[5] "Antaresia maculosa"  no data available (CCI 13/06/2018)
 
+str(db[db$Class %in% "MAMMALIA",])
+#5674 obs
+
+db.mam <- db[db$Class %in% c("MAMMALIA") & !is.na(db$male_body_mass_g) & !is.na(db$female_body_mass_g),]
+
+str(db.mam)
+
+db.mam [11:16, "binomial"]
+#[1] "Aethomys chrysophilus" #no mating sy (06/08/2018) PG no ASR ni marcaje
+#[2] "Ailurus fulgens"      pendiente asr
+#[3] "Akodon boliviensis"   Mating st no data asr pendiente ###
+#[4] "Akodon cursor"        
+#[5] "Alcelaphus buselaphus"
+#[6] "Alces alces" 
+
+
+
+
 
 
 
