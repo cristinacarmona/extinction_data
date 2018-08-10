dbcda1dd2e6927de7e91c531a4a18739f99c5d2e
Pepi Girón <peepig12@gmail.com>
2018-08-09 23:12:40 -0500

    se trabajó

--- Literature search/R/Fill database.R
+++ Literature search/R/Fill database.R
@@ -71,10 +71,10 @@ db.mam <- db[db$Class %in% c("MAMMALIA") & !is.na(db$male_body_mass_g) & !is.na(
 str(db.mam)
 
 db.mam [11:16, "binomial"]
-#[1] "Aethomys chrysophilus" #no mating sy (06/08/2018) PG no ASR ni marcaje sex di found
-#[2] "Ailurus fulgens"      
+#[1] "Aethomys chrysophilus" No ASR and mark dimo diferencia de peso, mating no encontredo ecepto un articulo que trabaja con sperma y suguiere un tipo de mating pero no lo menciona como tal
+#[2] "Ailurus fulgens"      no ASR
 #[3] "Akodon boliviensis"   Mating st no data NO asr### No data available
-#[4] "Akodon cursor"        
+#[4] "Akodon cursor"     no mating sy ASR pendiente##   
 #[5] "Alcelaphus buselaphus"
 #[6] "Alces alces" 
 
