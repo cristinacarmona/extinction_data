c942b04faba3e74457f2c779581c35529190cb41
mariapaulasidaoui <ma.paush97@gmail.com>
2018-08-22 19:56:04 -0500

    added info for 1 species ongoing

--- Literature search/R/Fill database.R
+++ Literature search/R/Fill database.R
@@ -3,33 +3,39 @@
 #########################################################
 #Load datasets
 
-setwd("C:/Users/cris.carmona/Documents/MEGAsync/Projects/Post-doc/Riesgo de extinción y selección sexual/extinction_data/Literature search/input")
+setwd("/Users/mariapaula/extinction_data/Literature search/input")
 
-csvfiles <- list.files(path = ".", pattern='*\\.csv$', all.files=TRUE)
-csvfiles
+xlsxfiles <- list.files(path = ".", pattern='*\\.xlsx$', all.files=TRUE)
 
-import.list <- lapply(csvfiles, read.csv, header = TRUE, as.is=TRUE, na.strings=c("NA","na","N","-","---"," ","",".","sin dato","SD","sd","Sin Dato", -999,"-999"))
+
+library(openxlsx) #instalar primero
+import.list <- lapply(xlsxfiles, read.xlsx, sheet=1, colNames =
+                        TRUE, na.strings=c("NA","na","N","-","---"," ","",".","sin
+                                           dato","SD","sd","Sin Dato", -999,"-999"))
 
 
 #str(import.list)
 ls()
 
-working.list <- import.list[2]
-names(working.list) <- c("db2")
+working.list <- import.list
+names(working.list) <- c("db")
 
 attach(working.list)
 ###########################################################
 
-names(db2)
-
+names(db)
+db2 <- db
 #Example with reptiles--------------
 #Choose Class to work with
 table(db2$Class)
 
 str(db2[db2$Class %in% "REPTILIA",]) #6278 obs
 
-db.rep <- db2[db2$Class %in% c("REPTILIA") & !is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm),]
+db.amp <- db2[db2$Class %in% c("AMPHIBIA") & !is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm),]
 
-str(db.rep)#161 obs
-
-db.rep[1:5,"binomial"]
+str(db.amp)#161 obs
+head(db.amp)
+db.amp[1:3,"binomial"]
+# [1] "Acris crepitans"
+# [2] "Acris gryllus"  
+# [3] "Adelotus brevis"
