27bcd7cc6cc47cf1821cf4ab87ba941f14be46c7
oscargarciama <oscar.garciama@udlap.mx>
2018-08-22 19:57:31 -0500

    added info for 1 species on going

--- Literature search/R/Fill database.R
+++ Literature search/R/Fill database.R
@@ -2,34 +2,42 @@
 #Fill in data into database
 #########################################################
 #Load datasets
+xlsxfiles <- list.files(path = ".", pattern='*\\.xlsx$', all.files=TRUE)
 
-setwd("C:/Users/cris.carmona/Documents/MEGAsync/Projects/Post-doc/Riesgo de extinción y selección sexual/extinction_data/Literature search/input")
 
-csvfiles <- list.files(path = ".", pattern='*\\.csv$', all.files=TRUE)
-csvfiles
-
-import.list <- lapply(csvfiles, read.csv, header = TRUE, as.is=TRUE, na.strings=c("NA","na","N","-","---"," ","",".","sin dato","SD","sd","Sin Dato", -999,"-999"))
+library(openxlsx) #instalar primero
+import.list <- lapply(xlsxfiles, read.xlsx, sheet=1, colNames =
+                        TRUE, na.strings=c("NA","na","N","-","---"," ","",".","sin
+                                           dato","SD","sd","Sin Dato", -999,"-999"))
 
 
 #str(import.list)
 ls()
 
-working.list <- import.list[2]
-names(working.list) <- c("db2")
+working.list <- import.list
+names(working.list) <- c("db")
 
 attach(working.list)
-###########################################################
 
+###########################################################
+db2 <- db
 names(db2)
 
 #Example with reptiles--------------
 #Choose Class to work with
 table(db2$Class)
 
-str(db2[db2$Class %in% "REPTILIA",]) #6278 obs
+str(db2[db2$Class %in% "ACTINOPTERYGII","CEPHALASPIDOMORPHI"," MYXINI",
+"CHONDRICHTHYES","SARCOPTERYGII",]) #6278 obs
 
-db.rep <- db2[db2$Class %in% c("REPTILIA") & !is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm),]
 
-str(db.rep)#161 obs
+db.fish <- db2[db2$Class %in% c("ACTINOPTERYGII","CEPHALASPIDOMORPHI"," MYXINI",
+"CHONDRICHTHYES","SARCOPTERYGII") & !is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm),]
 
-db.rep[1:5,"binomial"]
+str(db.fish)#161 obs
+head(db.fish)
+
+db.fish[1:3,"binomial"] 
+# "Agosia chrysogaster" Faltan comentarios y sex.dim y ASR(OGM 22/08/2018)  
+# "Campostoma anomalum"   
+# "Campostoma oligolepis"
