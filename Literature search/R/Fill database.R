646f31c25fb7b122718568664b45b7830e8b2c82
cristinacarmona <mcristina.carmona@gmail.com>
2018-09-21 11:21:34 -0500

    updated Rstudio file to include new species

--- Literature search/R/Fill database.R
+++ Literature search/R/Fill database.R
@@ -2,6 +2,8 @@
 #Fill in data into database
 #########################################################
 #Load datasets
+setwd("C:/Users/cris.carmona/Documents/MEGAsync/Projects/Post-doc/Riesgo de extinción y selección sexual/extinction_data/Literature search/input")
+
 xlsxfiles <- list.files(path = ".", pattern='*\\.xlsx$', all.files=TRUE)
 
 
@@ -27,17 +29,49 @@ names(db2)
 #Choose Class to work with
 table(db2$Class)
 
-str(db2[db2$Class %in% "ACTINOPTERYGII","CEPHALASPIDOMORPHI"," MYXINI",
-"CHONDRICHTHYES","SARCOPTERYGII",]) #6278 obs
+str(db2[db2$Class %in% c("ACTINOPTERYGII","CEPHALASPIDOMORPHI"," MYXINI",
+"CHONDRICHTHYES","SARCOPTERYGII"),]) #16333 obs
 
+str(db2[db2$Class %in% c("ACTINOPTERYGII","CEPHALASPIDOMORPHI"," MYXINI",
+                         "CHONDRICHTHYES","SARCOPTERYGII") & !is.na(db2$mating_system),])#939
+
+str(db2[db2$Class %in% c("ACTINOPTERYGII","CEPHALASPIDOMORPHI"," MYXINI",
+                         "CHONDRICHTHYES","SARCOPTERYGII") & 
+          ((!is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm)) | 
+             (!is.na(db2$male_size_cm.fishbase) & !is.na(db2$female_size_cm.fishbase))),]) #594 sp with known size for male and female
+
+str(db2[db2$Class %in% c("ACTINOPTERYGII","CEPHALASPIDOMORPHI"," MYXINI",
+                         "CHONDRICHTHYES","SARCOPTERYGII") & 
+          ((!is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm)) | 
+             (!is.na(db2$male_size_cm.fishbase) & !is.na(db2$female_size_cm.fishbase)) &
+             !is.na(db2$mating_system)),]) #164 species with known male and female size and Mating_system
 
 db.fish <- db2[db2$Class %in% c("ACTINOPTERYGII","CEPHALASPIDOMORPHI"," MYXINI",
-"CHONDRICHTHYES","SARCOPTERYGII") & !is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm),]
+                                "CHONDRICHTHYES","SARCOPTERYGII") & 
+                 ((!is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm)) | 
+                    (!is.na(db2$male_size_cm.fishbase) & !is.na(db2$female_size_cm.fishbase))),] #updated CCI 21/Sept/2018
 
-str(db.fish)#161 obs
+str(db.fish)#594 obs
 head(db.fish)
 
-db.fish[1:3,"binomial"] 
-# "Agosia chrysogaster" Faltan comentarios y sex.dim y ASR(OGM 22/08/2018)  
-# "Campostoma anomalum"   
-# "Campostoma oligolepis"
+db.fish[1:20,"binomial"] 
+# [1] "Abramis brama"            
+# [2] "Acanthobrama marmid"      
+# [3] "Acanthopagrus bifasciatus"
+# [4] "Acanthopagrus butcheri"   
+# [5] "Acanthopagrus schlegelii" 
+# [6] "Achoerodus gouldii"       
+# [7] "Acipenser oxyrinchus"     
+# [8] "Acipenser stellatus"      
+# [9] "Agosia chrysogaster"      
+# [10] "Albula vulpes"            
+# [11] "Alburnoides eichwaldii"   
+# [12] "Alburnus alburnus"        
+# [13] "Alburnus belvica"         
+# [14] "Alburnus chalcoides"      
+# [15] "Alepocephalus rostratus"  
+# [16] "Alestes baremoze"         
+# [17] "Alestes dentex"           
+# [18] "Alopias pelagicus"        
+# [19] "Alopias superciliosus"    
+# [20] "Alopias vulpinus"
