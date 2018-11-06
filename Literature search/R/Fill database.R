0cf2857934f3bb8b6abd3793722aadae282162ef
cristinacarmona <mcristina.carmona@gmail.com>
2018-11-06 12:31:23 -0600

    added missing species from pipoly

--- Literature search/R/Fill database.R
+++ Literature search/R/Fill database.R
@@ -110,3 +110,170 @@ amp[41:60,"binomial"]
 # [18] "Xenopus laevis"          No data available (CCI 05/11/2018)
 
 
+
+######################################################################################
+#Add species from Pipoly for which ASR was not added due to difference in sp names (use synonyms)
+
+#Load pipoly dataset
+
+setwd("C:/Users/cris.carmona/Documents/MEGAsync/Projects/Post-doc/Riesgo de extinción y selección sexual/Analysis/Bases de datos originales/")
+
+xlsxfiles <- list.files(path = ".", pattern='*\\.xlsx$', all.files=TRUE)
+
+
+library(openxlsx) #instalar primero
+import.list <- lapply(xlsxfiles, read.xlsx, sheet=1, colNames =
+                        TRUE, na.strings=c("NA","na","N","-","---"," ","",".","sin
+                                           dato","SD","sd","Sin Dato", -999,"-999"))
+
+
+#str(import.list)
+ls()
+
+working.list <- import.list[10]
+names(working.list) <- c("pipoly")
+
+attach(working.list)
+###########################################################
+
+names(pipoly)
+pip.noaves <- pipoly[pipoly$Taxon %in% c("mammal","amphibian", "reptile"),]
+str(pip.noaves) #157 obs
+str(pip.noaves[pip.noaves$Taxon %in% "amphibian",]) #39
+
+
+trim.trailing <- function (x) sub("\\s+$", "", x)
+db$Synonyms_nospace<-trim.trailing(db$Synonyms)
+
+intersect(pip.noaves$Scientific.name, db$binomial[db$Class %in% "AMPHIBIA"])#21 amphibians from pipoly in binomial sp
+pip.names.not.in.binomial<-setdiff(pip.noaves$Scientific.name[pip.noaves$Taxon %in% "amphibian"], db$binomial[db$Class %in% "AMPHIBIA"]) #18 sp from pipoly not in binomial sp
+
+
+c<-match.arg(pip.names.not.in.binomial, db$Synonyms_nospace, several.ok=T) #7 from the 18 not found in binomial are found in synonyms
+
+x<-pmatch(pip.names.not.in.binomial, db$Synonyms_nospace)#7
+db$Synonyms_nospace[x[!is.na(x)]]#7
+syn.inpip<-pip.names.not.in.binomial[!is.na(x)]
+
+#------debug, species that should appear using pmatch or match.arg:-------
+pip.names.not.in.binomial[!pip.names.not.in.binomial %in% syn.inpip]
+# [1] "Bufo marinus"          si
+# [2] "Bufo melanostictus"    si
+# [3] "Bufo viridis"          si
+# [4] "Hydromantes ambrosii"  no está
+# [5] "Hydromantes imperialis" no está 
+# [6] "Hydromantes italicus"  no está
+# [7] "Rana catesbeiana"      si
+# [8] "Rana nigromaculata"    si
+# [9] "Rana ridibunda"        si
+# [10] "Rana sphenocephala"   si
+# [11] "Triturus alpestris"   si
+
+#trial:
+trial<- "Ansonia kamblei  | Bufo melanostictus  |Bufo tienhoensis  "
+match.arg(pip.names.not.in.binomial, trial, several.ok=T)
+pmatch(pip.names.not.in.binomial, trial)
+
+#--------------------------------
+#Do it one by one for the 18 species not found in binomial names (4,5 and 6 not in db)-----
+db[grep(pip.names.not.in.binomial[1], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% "Bufo marinus"] <- "Rhinella marina"
+
+db[grep(pip.names.not.in.binomial[2], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[2]] <- db[grep(pip.names.not.in.binomial[2], db$Synonyms_nospace), c("binomial")]
+
+db[grep(pip.names.not.in.binomial[3], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% "Bufo viridis"] <- "Bufotes viridis"
+
+db[grep(pip.names.not.in.binomial[7], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[7]] <- db[grep(pip.names.not.in.binomial[7], db$Synonyms_nospace), c("binomial")]
+
+db[grep(pip.names.not.in.binomial[8], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[8]] <- db[grep(pip.names.not.in.binomial[8], db$Synonyms_nospace), c("binomial")]
+
+db[grep(pip.names.not.in.binomial[9], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[9]] <- db[grep(pip.names.not.in.binomial[9], db$Synonyms_nospace), c("binomial")]
+
+db[grep(pip.names.not.in.binomial[10], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[10]] <- db[grep(pip.names.not.in.binomial[10], db$Synonyms_nospace), c("binomial")]
+
+db[grep(pip.names.not.in.binomial[11], db$Synonyms_nospace), c("binomial","Synonyms")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[11]] <- "Pelophylax nigromaculatus"
+
+db[grep(pip.names.not.in.binomial[12], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[12]] <- db[grep(pip.names.not.in.binomial[12], db$Synonyms_nospace), c("binomial")]
+
+db[grep(pip.names.not.in.binomial[13], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[13]] <- db[grep(pip.names.not.in.binomial[13], db$Synonyms_nospace), c("binomial")]
+
+db[grep(pip.names.not.in.binomial[14], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[14]] <- db[grep(pip.names.not.in.binomial[14], db$Synonyms_nospace), c("binomial")]
+
+db[grep(pip.names.not.in.binomial[15], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[15]] <- db[grep(pip.names.not.in.binomial[15], db$Synonyms_nospace), c("binomial")]
+
+db[grep(pip.names.not.in.binomial[16], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[16]] <- db[grep(pip.names.not.in.binomial[16], db$Synonyms_nospace), c("binomial")]
+
+db[grep(pip.names.not.in.binomial[17], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[17]] <- db[grep(pip.names.not.in.binomial[17], db$Synonyms_nospace), c("binomial")]
+
+db[grep(pip.names.not.in.binomial[18], db$Synonyms_nospace), c("binomial")]
+pipoly$binomial[pipoly$Scientific.name %in% pip.names.not.in.binomial[18]] <- db[grep(pip.names.not.in.binomial[18], db$Synonyms_nospace), c("binomial")]
+
+
+#----------------------------
+#Add ASR to those 18 sp from pipoly with no ASR in db-----
+names(pipoly)
+add.asr <- pipoly[!is.na(pipoly$binomial), c("binomial","Adult.sex.ratio.*")]
+add.asr$ref.asr <- "pipoly"
+colnames(add.asr)<-c("binomial","ASR","ref.asr")
+
+db.pip <- merge(db, add.asr, by="binomial", all.x=T)
+head(db.pip)
+
+#merge ASR.x and ASR.y, ref.asr.x and ref.asr.y----
+db.pip[db.pip$ref.asr.x %in% "pipoly" | db.pip$ref.asr.y %in% "pipoly", c("binomial","ASR.x","ASR.y", "ref.asr.x", "ref.asr.y")]
+db.pip[db.pip$binomial %in% "Duttaphrynus melanostictus",]
+
+db.pip$ASR <- ifelse(is.na(db.pip$ASR.x) & !is.na(db.pip$ASR.y), db.pip$ASR.y, db.pip$ASR.x)
+db.pip[db.pip$ref.asr.x %in% "pipoly" | db.pip$ref.asr.y %in% "pipoly", c("ASR.x","ASR.y","ASR")]
+
+db.pip$ref.asr <- ifelse(is.na(db.pip$ref.asr.x) & !is.na(db.pip$ref.asr.y), db.pip$ref.asr.y, db.pip$ref.asr.x)
+db.pip[db.pip$ref.asr.x %in% "pipoly" | db.pip$ref.asr.y %in% "pipoly", c("ref.asr.x","ref.asr.y","ref.asr")]
+
+#get rid of extra columns
+names(db.pip)
+db.pip <- db.pip[,-c(55,56,62,63,64,65)]
+
+#-------------------------------------
+
+#check numbers of amphibians with data:
+db2 <- db.pip
+str(db2[db2$Class %in% "AMPHIBIA",]) #6609 obs
+
+db.amp <- db2[db2$Class %in% c("AMPHIBIA") & !is.na(db2$male_svl_cm) & !is.na(db2$female_svl_cm),]
+
+str(db.amp)#661 obs
+
+db.amp[!is.na(db.amp$ASR), "binomial"] #37
+db.amp[db.amp$ref.asr %in% "pipoly", "binomial"] #13
+db.amp[!db.amp$ref.asr %in% "pipoly" & !is.na(db.amp$ASR), "binomial"] #24
+
+db.amp[!is.na(db.amp$mating_system), "binomial"] #51
+db.amp[!is.na(db.amp$sex_dim), "binomial"] #47
+db.amp[!is.na(db.amp$sex_dim) | !is.na(db.amp$mating_system), "binomial"] #58
+db.amp[!is.na(db.amp$comments), "comments"] #58
+
+db.amp[!is.na(db.amp$sex_dim) | !is.na(db.amp$mating_system), c("comments","binomial") ] #58
+
+db2[c(40690, 40692, 35601, 23193),c("comments","binomial")]
+db2$comments[40690] <- "No data available (CCI 05/11/2018)"
+db2$comments[40692] <- "No data available (CCI 05/11/2018)"
+db2$comments[23193] <- "No data available (CCI 31/10/2018)"
+db2$comments[35601] <-  "No data available (CCI 05/11/2018)"
+
+#---------------------------
+#Save db.amp in excel file:--------
+setwd("C:/Users/cris.carmona/Documents/MEGAsync/Projects/Post-doc/Riesgo de extinción y selección sexual/extinction_data/Literature search/output/")
+write.csv(db.amp, "merged_iucn_and_db_v4_amphibians.csv")
